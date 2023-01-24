e40c7d632ff26e2b625255fbdb21d54d25f63950
osallahstevie <osallahstevie@gmail.com>
2023-01-24 16:46:22 +0300

    finished fetch crud lab

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,25 +1,45 @@
 import React from "react";
-
-function QuestionItem({ question }) {
+function QuestionItem({ question, onChangeOfAnswer, deleted }) {
   const { id, prompt, answers, correctIndex } = question;
-
+  
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
       {answer}
     </option>
   ));
-
+  const deleteFunc = () => {
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => deleted(question));
+  };
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={changeOfAnswer}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={deleteFunc}>Delete Question</button>
     </li>
   );
+  function changeOfAnswer(event) {
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        correctIndex: parseInt(event.target.value),
+      }),
+    })
+      .then((r) => r.json())
+      .then(() => onChangeOfAnswer(question));
+  }
 }
 
 export default QuestionItem;
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,43 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((quiz) => setQuestions(quiz));
+  }, []);
+  const deleteFunc = (deleted) => {
+    const newQue = questions.filter((quizes) => quizes.id !== deleted.id);
+    setQuestions(newQue);
+  };
+  const changeOfAnswer = (answer) => {
+    const newQuestions = questions.map((item) => {
+      if (item.id === answer.id) {
+        return answer;
+      }
+      return item;
+    });
+    setQuestions(newQuestions);
+  };
+  let quizzes = questions.map((quiz) => {
+    return (
+      <QuestionItem
+        onChangeOfAnswer={changeOfAnswer}
+        question={quiz}
+        deleted={deleteFunc}
+        key={quiz.id}
+      />
+    );
+  });
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{quizzes}</ul>
     </section>
   );
 }
 
-export default QuestionList;
+export default QuestionList
